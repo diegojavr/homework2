e772e9cd5676db0c01804c3c9e73f125c215c235
diegojavr <dgarcias@outlook.com>
2024-08-25 15:03:25 -0600

    feat: Todo app with support of add a task, delete a task and mark a task as completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# homework2
+# Homework 2
+- Build a Todo List using Vite with vanilla-ts. 
+- The application must support adding new items, deleting existing items, and marking items as completed. 
+- Use your creativity to create a UI with HTML and CSS to implement it. 
---
+++ todo-app/src/model/TaskItem.ts
@@ -0,0 +1,39 @@
+interface SingleTask {
+    id: string,
+    task: string,
+    completed: boolean
+}
+
+export default class TaskItem implements SingleTask {
+    constructor(
+        private _id: string = "",
+        private _task: string = "",
+        private _completed: boolean = false
+    ) { }
+
+    get id(): string {
+        return this._id;
+    }
+
+    set id(id: string) {
+        this._id = id;
+    }
+
+    get task(): string {
+        return this._task;
+    }
+
+    set task(task: string) {
+        this._task = task;
+    }
+
+    get completed(): boolean {
+        return this._completed;
+    }
+
+    set completed(completed: boolean) {
+        this._completed = completed;
+    }
+
+
+}
